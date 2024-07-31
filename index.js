3457198a7aed18f1c567607c04859fc6cd54d6e5
Andrew Aliaj <andyaliaj@gmail.com>
2024-07-31 12:37:37 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+
+server.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+server.listen(port, () => { 
+  console.log(`Server is running at http://localhost:${port}`);
+});
